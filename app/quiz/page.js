51fb521d7f0993ab374819b40998cb63f1fd68a8
AI_Worldview <dynhyper@gmail.com>
2026-07-19 12:21:01 +0800

    Update import path for questions.json

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import questions from '@/data/questions.json';
+import questions from '../../data/questions.json';
 
 export default function QuizPage() {
   const router = useRouter();
